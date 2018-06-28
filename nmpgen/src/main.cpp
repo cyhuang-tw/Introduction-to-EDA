e49e75a3ec2934297c76dbb32862cd2d77a3532b
cyhuang-tw <b04901147@ntu.edu.tw>
2018-06-28 23:15:30 +0800

    typo fixed

--- nmpgen/src/main.cpp
+++ nmpgen/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 #include "nmpMgr.h"
 
 nmpMgr mgr;
@@ -7,17 +8,25 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
-	if(argc != 2)
+	if(argc != 3)
 		return 0;
+
+	clock_t start, end;
 
 	string file = argv[1];
 	string outFile = argv[2];
+
+	start = clock();
 
 	if(!mgr.read(file))
 		return 0;
 
 	mgr.optimize();
 	mgr.printFile(outFile);
+
+	end = clock();
+
+	cout << "Elapsed time: " << ((double)(end - start))/CLOCKS_PER_SEC << endl;
 
 	return 0;
 }
